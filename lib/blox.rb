eb20067103831e47f9e9d833ce8d20d3b67d9f83
Arnaud Lachaume <arnaud.lachaume@welwire.com>
2014-06-01 17:35:57 +1000

    added config

--- lib/blox.rb
+++ lib/blox.rb
@@ -2,4 +2,25 @@ require 'blox/version'
 require 'blox/resource'
 
 module Blox
+  class << self
+    attr_accessor :config
+  end
+  
+  def self.configure(&block)
+    self.config ||= Configuration.new
+    yield(config) if block
+  end
+  
+  def self.root_path
+    self.configure unless self.config
+    self.config.root_path
+  end
+  
+  class Configuration
+    attr_accessor :root_path
+    
+    def initialize
+      @root_path = '/blox'
+    end
+  end
 end
---
+++ spec/blox_spec.rb
@@ -0,0 +1,29 @@
+require_relative 'spec_helper'
+
+describe Blox do
+  before(:each) do
+    Blox.config = nil
+  end
+  
+  describe "configuration" do
+    it "should have the right default config" do
+      Blox.configure
+      expect(Blox.config.root_path).to eq('/blox')
+    end
+    
+    it "should configure properly" do
+      Blox.configure { |c| c.root_path = '/bla' }
+      expect(Blox.config.root_path).to eq('/bla')
+    end
+  end
+  
+  describe "root_path" do
+    it "is a method" do
+      expect(Blox).to respond_to(:root_path)
+    end
+    
+    it "initializes the default config if not initialized" do
+      expect(Blox.root_path).to eq('/blox')
+    end
+  end
+end
